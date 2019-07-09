177a8b2887564ed3c161f4c0734b21bf55384b33
Darruma <darruma@protonmail.com>
2019-07-09 09:10:09 +0100

    Added plotting of images

--- Main.py
+++ Main.py
@@ -1,12 +1,14 @@
 import discord
 import datetime
+import io
 import json
 from discord.ext import commands
+import matplotlib.pyplot as plt
 
-bot = commands.Bot(command_prefix='>')
-bot.remove_command('help')
+bot = commands.Bot(command_prefix=">")
+bot.remove_command("help")
 
-f = open("token.txt", 'r')
+f = open("token.txt", "r")
 token = f.readline()
 token.strip()
 
@@ -15,94 +17,146 @@ token.strip()
 async def on_ready():
     print("Murcury Bot is ready!")
 
+
 @bot.command()
 async def help(ctx):
-	embed = discord.Embed(title="Murcury Bot", description="A bot for all things Murcury.", color=0xeee657)
-	embed.add_field(name=">alldebt", value="Shows the current total debt for all Murcury members.", inline=False)
-	embed.add_field(name=">adddebt @SENDER @RECIEVER X", value="Adds debt from @SENDER to @RECIVER by the ammount X" , inline=False)
-	embed.add_field(name=">register", value="Registers you on the system." , inline=False)
-	embed.add_field(name=">reset", value="Wipes entire system. **!USE AT OWN RISK!**" , inline=False)
-	await ctx.send(embed=embed)
-    
-#Creates a dictionary in the form USER:DEBT
-def fileToDict(fileName):
-	f = open(fileName, 'r')
-	diction = json.loads(f.read())
-	return diction
-	
+    embed = discord.Embed(
+        title="Murcury Bot", description="A bot for all things Murcury.", color=0xEEE657
+    )
+    embed.add_field(
+        name=">alldebt",
+        value="Shows the current total debt for all Murcury members.",
+        inline=False,
+    )
+    embed.add_field(
+        name=">adddebt @SENDER @RECIEVER X",
+        value="Adds debt from @SENDER to @RECIVER by the amount X",
+        inline=False,
+    )
+    embed.add_field(
+        name=">register", value="Registers you on the system.", inline=False
+    )
+    embed.add_field(
+        name=">reset", value="Wipes entire system. **!USE AT OWN RISK!**", inline=False
+    )
+    await ctx.send(embed=embed)
 
-#Writes dictionary to a file in the form USER,DEBT
+
+@bot.command()
+async def show_graph(ctx):
+    with open("history.json", "r") as h_json:
+        history = json.loads(h_json.read())
+        sender_name = str(ctx.message.author)
+        sender_history = history[sender_name]
+      
+        plt.scatter(range(len(sender_history)),sender_history)
+        plt.plot(range(len(sender_history)),sender_history,)
+        plt.gca().axes.get_xaxis().set_visible(False)
+        plt.ylabel("£ Money")
+        buffer = io.BytesIO()
+        plt.savefig(buffer,format="png")
+        buffer.seek(0)
+        await ctx.send(file=discord.File(buffer,"graph.png"))
+
+
+
+def fileToDict(fileName):
+    f = open(fileName, "r")
+    diction = json.loads(f.read())
+    return diction
+
+
+def updateHistory(new_info):
+    print(new_info)
+    with open("history.json", "r") as f:
+        # Load history file as json
+        dictionary = json.loads(f.read())
+        for user in dictionary:
+            for new_user, amount in new_info.items():
+                # Loop through history and current debt dict to find current user
+                if user == new_user:
+                    # Update the history with the users debt amount
+                    dictionary[user].push(amount)
+        dictToFile("history.json",dictionary)
+
+
+# Writes dictionary to a file in the form USER,DEBT
 def dictToFile(fileName, diction):
- 	f = open(fileName, 'w+')
- 	f.write(json.dumps(diction))
-	
+    f = open(fileName, "w+")
+    f.write(json.dumps(diction))
+
+
 @bot.command()
 async def debtArrows(ctx):
-	d = fileToDict("debt.txt")
-	arrows="```_Debt_\n"
-	for name, ammount in d.items():
-		
-		if(ammount < 0):
-			for name2, ammount2 in d.items():
-				if(name!=name2):
-					if(ammount2>0):
-						arrows = arrows + "\n"+name+" --> "+name2+" "+str(ammount2)
-						d[name] = int(d[name]) + int(ammount2)
-						d[name2] = int(d[name2]) - int(ammount2)
-						
-	await ctx.send(arrows+"```")	
-	
+    d = fileToDict("debt.json")
+    arrows = "```_Debt_\n"
+    for name, amount in d.items():
+        if amount < 0:
+            for name2, amount2 in d.items():
+                if name != name2:
+                    if amount2 > 0:
+                        arrows = (
+                            arrows + "\n" + name + " --> " + name2 + " " + str(amount2)
+                        )
+                        d[name] = int(d[name]) + int(amount2)
+                        d[name2] = int(d[name2]) - int(amount2)
+
+    await ctx.send(arrows + "```")
+
 
 @bot.command()
 async def alldebt(ctx):
-	d = fileToDict("debt.txt")
-	content=""
-	if(d):
-		for name, ammount in d.items():
-			content = content + str(name)+": "+str(ammount)
-		
-	else:
-		content = "There is no debt currently on the system."
-		
-	await ctx.send(content)
+    d = fileToDict("debt.json")
+    content = ""
+    if d:
+        for name, amount in d.items():
+            content = content + str(name) + ": " + str(amount)
+    else:
+        content = "There is no debt currently on the system."
+
+    await ctx.send(content)
+
 
 @bot.command()
-async def adddebt(ctx, sender : discord.User, reciever : discord.User, ammount : int):
-	if(sender.id != reciever.id):
-		if(ammount <=0):
-			await ctx.send("Cannot have zero or negative debt!")
-		else:
-			d = fileToDict("debt.txt")
-			if((str(sender) in d) != True ):
-				d[str(sender)] = "0"
-			if((str(reciever) in d) != True ):
-				d[str(reciever)] = "0"
+async def adddebt(ctx, sender: discord.User, reciever: discord.User, amount: int):
+    if sender.id != reciever.id:
+        if amount <= 0:
+            await ctx.send("Cannot have zero or negative debt!")
+        else:
+            d = fileToDict("debt.json")
+            if (str(sender) in d) != True:
+                d[str(sender)] = "0"
+            if (str(reciever) in d) != True:
+                d[str(reciever)] = "0"
 
-			d[str(sender)] = int(d[str(sender)]) - int(ammount)
-			d[str(reciever)] =  int(d[str(reciever)]) + int(ammount) 
-			
-	dictToFile("debt.txt",d)
-	await ctx.send(f'{sender.mention} now owes {ammount} to {reciever.mention}')
-	
+            d[str(sender)] = int(d[str(sender)]) - int(amount)
+            d[str(reciever)] = int(d[str(reciever)]) + int(amount)
+
+    dictToFile("debt.json", d)
+    updateHistory(d)
+    await ctx.send(f"{sender.mention} now owes {amount} to {reciever.mention}")
+
 
 @bot.command()
 async def reset(ctx):
-	dictToFile("debt.txt",{})
+    dictToFile("debt.json", {})
+
 
 @bot.command()
 async def register(ctx):
-	d = fileToDict("debt.txt")
-	name = str(ctx.message.author)
-	if(name in d):
-		await ctx.send(name+" has already been registered.")
-	else:
-		print(d)
-		d[name] = "0"
-		dictToFile("debt.txt",d)
-		print(d)
-		await ctx.send(name+" has been registered.")
-	
-	
+    d = fileToDict("debt.json")
+    history = fileToDict("history.json")
+    name = str(ctx.message.author)
+    if name in d or name in history:
+        await ctx.send(name + " has already been registered.")
+    else:
+        print(d)
+        d[name] = "0"
+        history[name] = []
+        dictToFile("debt.json", d)
+        dictToFile("history.json", history)
+        print(d)
+        await ctx.send(name + " has been registered.")
 
 
 bot.run(token)
